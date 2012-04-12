2645f73e368da1766c0f387b877cee6ddd5410cd
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2012-04-12 11:22:02 +0100

    First pass at the simple interface

--- src/main/java/uk/ac/shef/dcs/oak/tasks/App.java
+++ src/main/java/uk/ac/shef/dcs/oak/tasks/App.java
@@ -1,13 +1,34 @@
 package uk.ac.shef.dcs.oak.tasks;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
- * Hello world!
- *
+ * Class for showing the current task from a google calendar
  */
-public class App 
+public class App extends JFrame
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+   String currentTask = "No Task";
+
+   public App()
+   {
+      initGUI();
+   }
+
+   JLabel label;
+
+   private void initGUI()
+   {
+      label = new JLabel(currentTask);
+      this.add(label);
+      this.pack();
+   }
+
+   public static void main(String[] args)
+   {
+      App mine = new App();
+      mine.setLocationRelativeTo(null);
+      mine.setVisible(true);
+      mine.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
 }
